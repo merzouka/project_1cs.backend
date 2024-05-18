805ce8cf46b8f1f2b2cbd2940f7e5f4c5bc38cf5
merzouka <marzoukayouness@gmail.com>
2024-05-18 14:49:58 +0100

    Changes: finished seeders for registration, users, user_baladiyas

--- EL_Hajj/utils/pass_gen.py
+++ EL_Hajj/utils/pass_gen.py
@@ -2,7 +2,7 @@ from django.contrib.auth.hashers import make_password
 
 password = "Hello2003"
 # admin, doctor, drawing_manager, payment_manager, user, hajj
-accounts = 1 + 10 + 10 + 10 + 50 + 35
+accounts = 1 + 5 + 5 + 5 + 50 + 35
 file_path = "/home/merzouka/code/hajj/backend/EL_Hajj/utils/seed/passwords.txt"
 
 def gen_pass_file():
